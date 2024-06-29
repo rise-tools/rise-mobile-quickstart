8f182f259ebde7287c551b1d4d554832b82a83bc
Eric Vicenti <eric@vicenti.net>
2024-06-29 13:11:33 +0200

    feat: rise tools example

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,39 @@
-import { Button, YStack } from 'tamagui';
+import { Rise } from '@rise-tools/react'
+import { createWSModelSource } from '@rise-tools/ws-client';
+import {
+  FormComponents,
+  LucideIconsComponents,
+  QRCodeComponents,
+  SVGComponents,
+  TamaguiComponents,
+  useHapticsActions,
+  useLinkingActions,
+  useToastActions,
+  RiseComponents,
+} from '@rise-tools/kitchen-sink'
+
+const components = {
+  ...TamaguiComponents,
+  ...RiseComponents,
+  ...FormComponents,
+  ...SVGComponents,
+  ...LucideIconsComponents,
+  ...QRCodeComponents,
+}
+
+const modelSource = createWSModelSource('ws://localhost:8888');
 
 export default function HomeScreen() {
   return (
-    <YStack>
-      <Button>Hello</Button>
-    </YStack>
+    <Rise
+      modelSource={modelSource}
+      components={components}
+      path="incrementer"
+      actions={{
+        ...useToastActions(),
+        ...useHapticsActions(),
+        ...useLinkingActions(),
+      }}
+    />
   );
 }
