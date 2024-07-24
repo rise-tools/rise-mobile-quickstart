023d0c0214b1dc563b2b6c289a0ea27d2741b821
Eric Vicenti <eric@vicenti.net>
2024-07-24 23:20:00 +0200

    Change port to new server default

--- src/modelSource.ts
+++ src/modelSource.ts
@@ -1,3 +1,3 @@
 import { createWSModelSource } from "@rise-tools/ws-client";
 
-export const modelSource = createWSModelSource("ws://localhost:8888");
+export const modelSource = createWSModelSource("ws://localhost:3005");
